0abb8597568a559755ee521692e8eae9e155c678
Alex <alexander.staehle@htwg-konstanz.de>
2022-02-14 12:04:59 +0100

    Fix dataloader parameter + formatting

--- cnn.py
+++ cnn.py
@@ -20,8 +20,8 @@ batch_size = 32
 
 def load_mnist(batch_size=32, seed=42):
     torch.manual_seed(seed)
-    
-    mnist_train = dsets.MNIST(root="data/MNIST", 
+
+    mnist_train = dsets.MNIST(root="data/MNIST",
                               train=True,
                               transform=transforms.ToTensor(),
                               download=True)
@@ -29,46 +29,45 @@ def load_mnist(batch_size=32, seed=42):
                              train=False,
                              transform=transforms.ToTensor(),
                              download=True)
-    
-    test_dataloader = torch.utils.data.DataLoader(dataset=mnist_test, 
-                                              shuffle=True,
-                                              batch_size=batch_size)
+
+    test_dataloader = torch.utils.data.DataLoader(dataset=mnist_test,
+                                                  shuffle=True,
+                                                  batch_size=batch_size)
     train_dataloader = torch.utils.data.DataLoader(dataset=mnist_train,
                                                    shuffle=True,
                                                    batch_size=batch_size)
-    
-    return train_dataloader, test_dataloader
 
+    return train_dataloader, test_dataloader
 
 
 # train_dataloader, test_dataloader = load_mnist()
 train_dataloader, test_dataloader = load_mnist(batch_size=batch_size)
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
 class CNN_MNIST(nn.Module):
-    def __init__(self, dropout_prob = 0.5):
+    def __init__(self, dropout_prob=0.5):
         super(CNN_MNIST, self).__init__()
-        
+
         self.layer_1 = nn.Sequential(
-            nn.Conv2d(1,  32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(p=dropout_prob))
-        
+
         self.layer_2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(p=dropout_prob))
-        
+
         self.flatten = nn.Flatten()
-        
-        self.output_layer = nn.Linear(7*7*64, 10, bias=True)
-        
+
+        self.output_layer = nn.Linear(7 * 7 * 64, 10, bias=True)
+
         self.softmax = nn.Softmax(dim=1)
-                
+
     def forward(self, x):
         x = self.layer_1(x)
         x = self.layer_2(x)
@@ -76,14 +75,14 @@ class CNN_MNIST(nn.Module):
         x = self.flatten(x)
         x = self.output_layer(x)
         output = self.softmax(x)
-        
+
         return output
-        
-    
+
+
 # def optimize_step(model, optimizer, criterion, learning_rate):
-    
-    
-def optimize(train_datalaoder, test_dataloader, batch_size, model, optimizer, criterion, learning_rate, epochs):
+
+
+def optimize(train_dataloader, test_dataloader, batch_size, model, optimizer, criterion, learning_rate, epochs):
     train_cost = []
     train_acc = []
 
@@ -91,38 +90,39 @@ def optimize(train_datalaoder, test_dataloader, batch_size, model, optimizer, cr
 
     for epoch in range(epochs):
         avg_cost = 0
-        
+
         for i, (batch_X, batch_Y) in enumerate(train_dataloader):
             X, Y = Variable(batch_X), Variable(batch_Y)
-                        
+
             optimizer.zero_grad()
-            
+
             hypothesis = model(X)
             cost = criterion(hypothesis, Y)
-            
+
             cost.backward()
             optimizer.step()
-            
+
             prediction = hypothesis.data.max(dim=1)[1]
             train_acc.append(((prediction.data == Y.data).float().mean()).item())
             train_cost.append(cost.item())
-            
+
             if i % 200 == 0:
-                print("Epoch= {},\t batch = {},\t cost = {:2.4f},\t accuracy = {}".format(epoch+1, i, train_cost[-1], train_acc[-1]))
-           
+                print("Epoch= {},\t batch = {},\t cost = {:2.4f},\t accuracy = {}".format(epoch + 1, i, train_cost[-1],
+                                                                                          train_acc[-1]))
+
             avg_cost += cost.data / total_batch
 
         print("[Epoch: {:>4}], averaged cost = {:>.9}".format(epoch + 1, avg_cost.item()))
 
-
     print('Learning Finished!')
-    
+
+
 model = CNN_MNIST()
-    
+
 learning_rate = 1e-3
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
 
 training_epochs = 1
 
-optimize(test_dataloader, test_dataloader, batch_size, model, optimizer, criterion, learning_rate, training_epochs)
+optimize(train_dataloader, test_dataloader, batch_size, model, optimizer, criterion, learning_rate, training_epochs)
